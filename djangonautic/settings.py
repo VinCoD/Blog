e94d3246975a83d5e9d38157f045e68705bbed47
VinCoD <5895.2020@students.ku.ac.ke>
2022-02-18 22:32:37 +0300

    Added root template directory

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'djangonautic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
