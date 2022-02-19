7cc4d279c4d7518f46b08250f1e20f369723ed9f
VinCoD <5895.2020@students.ku.ac.ke>
2022-02-19 18:00:38 +0300

    Removed all unnecessary images from before

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,7 @@
 from djangonautic.settings import TIME_ZONE
 from django.db import models
 from django.db.models.fields import CharField
+from django.contrib.auth.models import User
 
 
 class Article(models.Model):
@@ -9,7 +10,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
-    # add in author later
+    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -17,5 +17,13 @@ def article_detail(request, slug):
 
 @login_required(login_url='/accounts/login')
 def article_create(request):
-    form = forms.CreateArticle()
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid:
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
     return render(request, 'articles/article_create.html', {'form': form})
